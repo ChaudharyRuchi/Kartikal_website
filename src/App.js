b700c37fb619c97678a62d8b425e69bf484529c4
harsh.singh@borderfree.io-at-297767201074 <harsh.singh@borderfree.io>
2023-02-10 15:39:49 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -73,106 +73,173 @@ function App() {
       </div>
 
       <div className="div-2">
+        <div className="div2-desc">
+          <h1 className="head-overview">Overview</h1>
+          <p className="orange-line"></p>
+          <p className="para-overview">
+            <span>A pen test</span> also known as a penetration test is a
+            security assessment of an IT systems. where potential vulnerabilites
+            are identified by attempteing to exploit them.It can be doing using
+            human or automated methods and may include testing servers, web
+            apps, wireless networks, and other areas of potential exposure.The{" "}
+            <span>NIST SP 800-115 guide</span> is often used to develop
+            appropriate security processes and procedure and can be a helpful
+            resources for pen testers when assessing organizational
+            vulnerabilites
+          </p>
+        </div>
+
         <img src="Assets\Overview.webp" className="div-2-overview"></img>
-        <h3 className="head-overview">Overview</h3>
-        <p className="para-overview">
-          A pen test also known as a penetration test is a security assessment
-          of an IT systems. where potential vulnerabilites are identified by
-          attempteing to exploit them.It can be doing using human or automated
-          methods and may include testing servers, web apps, wireless networks,
-          and other areas of potential exposure.The NIST SP 800-115 guide is
-          often used to develop appropriate security processes and procedure and
-          can be a helpful resources for pen testers when assessing
-          organizational vulnerabilites
-        </p>
       </div>
 
       <div className="div-3">
-        <img src="Assets\Why Kratikal.webp"></img>
-        <h1>Why Kratikal?</h1>
-        <p>
-          Kratikal Tech offers manual and automated penetration testing services
-          to identify vulnerabilitesin your systems. Our manualtesting team uses
-          techniques like social engineering, phoshing, and physical security
-          testing for a more in-depth evaluation. Our automated testing approach
-          uses software tools to simulate an attack, but may not delete all
-          vulnerabilites. Together, both methods provide a comprehensive and
-          thorough security assessment.
-        </p>
-        <h1>Why Choose us ?</h1>
-        <ul>
-          <li>Creating compliance Policies and IT Security Strategies</li>
-          <li>Sound record of delivering excellent cyber security solutions</li>
-          <li>Precise Gap Analysis by our expert Pentesters</li>
-          <li>thorough Security assessment</li>
-          <li>Safeguard people, process, and technologies</li>
-        </ul>
+        <div className="top-div">
+          <div className="left-div">
+            <img src="Assets\Why Kratikal.webp"></img>
+          </div>
+          <div className="right-div">
+            <h1>Why Kratikal?</h1>
+            <p className="orange-line"></p>
+            <p>
+              Kratikal Tech offers manual and automated penetration testing
+              services to identify vulnerabilitesin your systems. Our
+              manualtesting team uses techniques like social engineering,
+              phoshing, and physical security testing for a more in-depth
+              evaluation. Our automated testing approach uses software tools to
+              simulate an attack, but may not delete all vulnerabilites.
+              Together, both methods provide a comprehensive and thorough
+              security assessment.
+            </p>
+          </div>
+        </div>
+        <div className="bottom-div">
+          <div className="left-div">
+            <h1>Why Choose us ?</h1>
+            <p className="orange-line"></p>
+            <ul>
+              <li>Creating compliance Policies and IT Security Strategies</li>
+              <li>
+                Sound record of delivering excellent cyber security solutions
+              </li>
+              <li>Precise Gap Analysis by our expert Pentesters</li>
+              <li>thorough Security assessment</li>
+              <li>Safeguard people, process, and technologies</li>
+            </ul>
+          </div>
+          <div className="right-div">
+            <img src="Assets\Why Choose us.webp" className="middle-image"></img>
+          </div>
+        </div>
+
         <button>Book a call</button>
-        <img src="Assets\Why Choose us.webp" className="middle-image"></img>
       </div>
 
       <div className="div-4">
         <h1>Benefits associated with code Review</h1>
-        <img src="Assets\icon01.webp"></img>
-        <p>Cost Saving</p>
-        <img src="Assets\icon02.webp"></img>
-        <p>Improve Code Quality</p>
-        <img src="Assets\icon03.webp"></img>
-        <p>Identify and Fix Vulnerabilites</p>
-        <img src="Assets\icon04.webp"></img>
-        <p>Maintaing Consistency</p>
-        <img src="Assets\icon05.webp"></img>
-        <p>Detection of Errors</p>
+        <p className="orange-line"></p>
+        <div className="icons-div">
+          <div className="icon-tile">
+            <img src="Assets\icon01.webp"></img>
+            <p>Cost Saving</p>
+          </div>
+          <div className="icon-tile">
+            <img src="Assets\icon02.webp"></img>
+            <p>Improve Code Quality</p>
+          </div>
+          <div className="icon-tile">
+            <img src="Assets\icon03.webp"></img>
+            <p>Identify and Fix Vulnerabilites</p>
+          </div>
+          <div className="icon-tile">
+            <img src="Assets\icon04.webp"></img>
+            <p>Maintaing Consistency</p>
+          </div>
+          <div className="icon-tile">
+            <img src="Assets\icon05.webp"></img>
+            <p>Detection of Errors</p>
+          </div>
+        </div>
       </div>
 
       <div className="div-5">
-        <h1>Code Review</h1>
-        <p>
-          Code Review examines source code directcly and reveals flaws that were
-          missed during the first dvelopment stage. It is the procedure of
-          auditing an application's source code to make sure the necessary
-          security measures are present, performing as intended, and having been
-          activated appropriately.
-        </p>
-        <p>
-          They are methodical evaluationof the code made to find defects,
-          improve the quality of the code, and assist dvelopers in learning the
-          source code
-        </p>
-        <img src="Assets\Code Review.webp"></img>
+        <div className="left-div">
+          <h1>Code Review</h1>
+          <p className="orange-line"></p>
+          <p>
+            Code Review examines source code directcly and reveals flaws that
+            were missed during the first dvelopment stage. It is the procedure
+            of auditing an application's source code to make sure the necessary
+            security measures are present, performing as intended, and having
+            been activated appropriately.
+          </p>
+          <p>
+            They are methodical evaluationof the code made to find defects,
+            improve the quality of the code, and assist dvelopers in learning
+            the source code
+          </p>
+        </div>
+        <div className="right-div">
+          <img src="Assets\Code Review.webp"></img>
+        </div>
       </div>
       <div className="div-6">
-        <h1>FAQ's</h1>
-        <h3> What are the Benefits of Penetration Testing</h3>
-        <p>
-          With the help of penetrationtesting, you can quickly identify your
-          security holes and take steps to close them before a hacker usesthem
-          against you. For companies of all sizes , penetration testing is very
-          advantageous:
-        </p>
-        <ul>
-          <li>Prevent data breaches</li>
-          <li>Follow the best security practices</li>
-          <li>Enhance your reputation</li>
-          <li>Meet Compliance</li>
-        </ul>
+        <div className="heading">
+          <p className="orange-line"></p>
+          <h1>FAQ's</h1>
+          <p className="orange-line"></p>
+        </div>
 
-        <h2>How does a penetrationtest work</h2>
-        <h2>What are the factors while performingpen testing</h2>
+        <div className="description">
+          <h3> What are the Benefits of Penetration Testing</h3>
+          <div className="sub-content">
+            <p>
+              With the help of penetrationtesting, you can quickly identify your
+              security holes and take steps to close them before a hacker
+              usesthem against you. For companies of all sizes , penetration
+              testing is very advantageous:
+            </p>
+            <ul>
+              <li>Prevent data breaches</li>
+              <li>Follow the best security practices</li>
+              <li>Enhance your reputation</li>
+              <li>Meet Compliance</li>
+            </ul>
+          </div>
+
+          <h3>How does a penetrationtest work</h3>
+          <h3>What are the factors while performingpen testing</h3>
+        </div>
       </div>
       <div className="footer">
-        <p className="footer-text-1">Contact Us</p>
-        <p className="footer-text-2">UNITED STATES</p>
-        <p className="footer-text-3">
-          400 W Peachtree St NW Arlanta, GA, 30308, USA
-        </p>
-        <p className="footer-text-4">INDIA</p>
-        <p className="footer-text-5">B-70, SEcond floor-67, Noida-201301</p>
-
-        <p className="footer-text-6">For Sales:</p>
-        <p className="footer-text-7">
-          Email: sales@kratikal.companies (+91)9289192210
-        </p>
+        <div className="footer-heading">
+          <h1 className="background-text">CONTACT US</h1>
+          <div className="sub-heading">
+            <h4 className="footer-text-1">CONTACT US</h4>
+            <p className="sub-heading-underline">
+              <p className="grey-line"></p>
+              <p className="pink-line"></p>
+              <p className="grey-line"></p>
+            </p>
+          </div>
+        </div>
+        <div className="contact-tiles">
+          <div className="contact-tile">
+            <p className="footer-text-2">UNITED STATES</p>
+            <p className="footer-text-3">
+              400 W Peachtree St NW Arlanta, GA, 30308, USA
+            </p>
+          </div>
+          <div className="contact-tile">
+            <p className="footer-text-4">INDIA</p>
+            <p className="footer-text-5">B-70, SEcond floor-67, Noida-201301</p>
+          </div>
+          <div className="contact-tile">
+            <p className="footer-text-6">For Sales:</p>
+            <p className="footer-text-7">
+              Email: sales@kratikal.companies (+91)9289192210
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
